c548ea2fe448692ffcc17afe2e50efa519982a43
Blake Loring <blake@parsed.uk>
2021-01-29 09:29:31 +0800

    Nearly working (UDP)

--- native/src/main.cpp
+++ native/src/main.cpp
@@ -18,6 +18,15 @@
 #include <map>
 #include <netinet/udp.h>
 
+void binddev(int fd) {
+  struct ifreq ifr;
+  memset(&ifr, 0, sizeof(ifr));
+  snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), "wlp2s0");
+  if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr, sizeof(ifr)) < 0) {
+    printf("Woof\n");
+  }
+}
+
 int guard(int r) {
   if (r < 0) {
     fprintf(stderr, "Guard was violated\n");
@@ -44,14 +53,15 @@ void listen_epollin(int epoll_fd, int fd) {
 #define MTU 1500
 #define MAX_EVENTS 500
 
-#define DROP_GUARD(e) if (!e) { \
-  printf("Dropped Packet (DROP GUARD)\n"); \
+#define DROP_GUARD(e) if (!(e)) { \
+  printf("Dropped Packet (%i) (DROP GUARD) %i\n", e, __LINE__); \
+  perror("Reason: "); \
   return; \
 }
 
 struct ip {
-    uint8_t version : 4;
     uint8_t ihl : 4;
+    uint8_t version : 4;
     uint8_t tos;
     uint16_t len;
     uint16_t id;
@@ -66,21 +76,31 @@ struct ip {
 
 struct ip_port {
   uint32_t ip;
-  uint16_t port;
+  uint16_t src_port;
+  uint16_t dst_port;
 
   // Impl comparison to allow use in a map
   bool operator<(const ip_port &o) const {
-    if (this->ip == o.ip && this->port == o.port) {
-      return 0;
-    } else {
-      return -1;
-    }
+    return memcmp(this, &o, sizeof(ip_port));
   }
+};
+
+enum FDType {
+  UDP = 0,
+  TCP,
+};
+
+struct msg_return {
+  int fd;
+  uint32_t src_ip;
+  uint16_t src_port;
+  FDType type;
 };
 
 typedef struct event_loop {
   int epoll_fd;
-  std::map<ip_port, int> udp_pairs;
+  std::map<ip_port, msg_return> udp_pairs;
+  std::map<int, msg_return> udp_return;
 } event_loop_t;
 
 void process_packet_icmp(struct ip* hdr, char* bytes, size_t len) {
@@ -94,23 +114,28 @@ void process_packet_udp(event_loop_t* loop, struct ip* hdr, char* bytes, size_t 
   DROP_GUARD(len >= sizeof(struct udphdr));
 
   struct udphdr* udp_hdr = (struct udphdr *) bytes;
-  struct ip_port id = { hdr->daddr, udp_hdr->uh_sport };
+  struct ip_port id = { hdr->daddr, udp_hdr->uh_sport, udp_hdr->uh_dport };
+
+  printf("%i %i %i\n", hdr->daddr, udp_hdr->uh_sport, udp_hdr->uh_dport);
 
   auto fd_scan = loop->udp_pairs.find(id);
 
   int found_fd;
 
   if (fd_scan != loop->udp_pairs.end()) {
-    found_fd = (*fd_scan).second;
+    found_fd = (*fd_scan).second.fd;
   } else {
     // No known UDP socket, open a new one
 
     // Create a new UDP FD
     int new_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    binddev(new_fd);
+    printf("New FD: %i %i\n", new_fd, new_fd >= 0);
     DROP_GUARD(new_fd >= 0);
 
     // Register it
-    loop->udp_pairs[id] = new_fd;
+    loop->udp_pairs[id] = { new_fd, hdr->saddr, udp_hdr->uh_sport, UDP };
+    loop->udp_return[new_fd] = loop->udp_pairs[id];
     listen_epollin(loop->epoll_fd, new_fd);
     set_nonblocking(new_fd);
 
@@ -127,7 +152,9 @@ void process_packet_udp(event_loop_t* loop, struct ip* hdr, char* bytes, size_t 
   dst.sin_addr.s_addr = hdr->daddr;
   dst.sin_port = udp_hdr->uh_dport;
 
+  printf("Trying to send a UDP packet\n");
   DROP_GUARD(sendto(found_fd, bytes, len, 0, (struct sockaddr*) &dst, sizeof(dst)) >= 0);
+  printf("Sent UDP packet\n");
 }
 
 void process_tun_packet(event_loop_t* loop, char bytes[MTU], size_t len) {
@@ -137,6 +164,7 @@ void process_tun_packet(event_loop_t* loop, char bytes[MTU], size_t len) {
   struct ip* hdr = (struct ip*) bytes;
 
   // TODO: Add IPv6 later once we get this IPv4 thing worked out
+  printf("IP HDR Version: %i %i\n", hdr->version, hdr->ihl);
   DROP_GUARD(hdr->version == 4);
 
   char* rest = bytes + sizeof(struct ip);
@@ -149,9 +177,10 @@ void process_tun_packet(event_loop_t* loop, char bytes[MTU], size_t len) {
     }
     case 17: {
       process_packet_udp(loop, hdr, rest, len);
+      break;
     }
     default: {
-      printf("Unsupported IP/protocol; dropped\n");
+      printf("Unsupported IP/protocol; dropped %i\n", hdr->proto);
       return;
     }
   }
@@ -180,6 +209,17 @@ void user_space_ip_proxy(int tunnel_fd) {
         ssize_t readb = read(tunnel_fd, bytes, MTU);
         if (readb > 0) {
           process_tun_packet(&loop, bytes, readb);
+        }
+      } else {
+        printf("other FD read\n");
+        char buf[65536];
+        sockaddr_in addr;
+        socklen_t addr_len = sizeof(addr);
+        ssize_t len = recvfrom(events[i].data.fd, buf, 65536, 0, (sockaddr *) &addr, &addr_len);
+        printf("Read from %i %i a %li byte response\n", addr.sin_addr.s_addr, addr.sin_port, len);
+        auto fd_scan = loop.udp_return.find(events[i].data.fd);
+        if (fd_scan != loop.udp_return.end()) {
+          printf("Found a return path\n");
         }
       }
     }
